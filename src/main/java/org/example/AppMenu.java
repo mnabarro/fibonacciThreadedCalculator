3ea4723ca06e2b69e02886a7faea43966cb3b62f
Matias Nabarro <matias.nabarro@clarity.ai>
2023-02-15 11:59:08 +0100

    add menu related methods

---
+++ src/main/java/org/example/AppMenu.java
@@ -0,0 +1,49 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class AppMenu {
+
+  class MenuMessages {
+    public final static String chooseValidOption = "Sorry, you have to choose one valid option";
+    public static final String pressEnterToContinue = "Press <ENTER> to continue";
+  }
+  class MenuOptions {
+
+    public static final String showTaskList = "1";
+    public static final String addNewTask = "2";
+    public static final String startTask = "3";
+    public static final String seeResults = "4";
+    public static final String quitProgram = "Q";
+  }
+
+  public static String getUserInput() {
+    Scanner userInput = new Scanner(System.in);
+    return userInput.nextLine();
+  }
+  public static String mainMenu() {
+    return new StringBuilder()
+      .append("\n\n")
+      .append("1 - Show tasks list\n")
+      .append("2 - Add a new task\n")
+      .append("3 - Start a task\n")
+      .append("4 - See results of completed tasks\n")
+      .append("Q - Quit program\n")
+      .toString();
+  }
+
+  public static String addTaskMenu() {
+    return new StringBuilder()
+      .append("\n")
+      .append("Enter the number to calculate\n")
+      .toString();
+  }
+
+  public static String startTaskMenu() {
+    return new StringBuilder()
+      .append("\n")
+      .append("Enter the task number to start\n")
+      .toString();
+  }
+
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,11 @@ package org.example;
 
 import java.util.ArrayList;
 import java.util.Random;
+import org.example.AppMenu.MenuOptions;
 
 public class Main {
 
+  static String selection = "";
   static ArrayList<MyThread> threadsCollection = new ArrayList<>();
   static int getRandomIntInRange(int min, int max) {
     Random random = new Random();
@@ -12,7 +14,19 @@ public class Main {
   }
   public static void main(String[] args) {
 
-  threadsCollection.add(new MyThread("Thread #"+ threadsCollection.size(), getRandomIntInRange(5,50)));
-  threadsCollection.get(0).start();
+    System.out.println(AppMenu.mainMenu());
+    do {
+      selection = AppMenu.getUserInput();
+      if (selection.equals(MenuOptions.quitProgram)) {
+        System.exit(0);
+      } else if (selection.equals(MenuOptions.addNewTask)) {
+        AppMenu.addTaskMenu();
+      }
+
+
+    } while (true);
+
+//  threadsCollection.add(new MyThread("Thread #"+ threadsCollection.size(), getRandomIntInRange(5,50)));
+//  threadsCollection.get(0).start();
   }
 }
